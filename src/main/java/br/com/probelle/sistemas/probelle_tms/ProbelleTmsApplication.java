b22ae89f7ca2e6863400261b9dcb5307327d8626
Emanuel Castro <emanuel.castro@probelle.com.br>
2026-02-05 13:54:35 -0300

    first commit

---
+++ src/main/java/br/com/probelle/sistemas/probelle_tms/ProbelleTmsApplication.java
@@ -0,0 +1,13 @@
+package br.com.probelle.sistemas.probelle_tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbelleTmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbelleTmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/probelle/sistemas/probelle_tms/ProbelleTmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.probelle.sistemas.probelle_tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbelleTmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
